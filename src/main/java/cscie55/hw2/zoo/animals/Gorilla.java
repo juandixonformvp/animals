8aa9fde472a8675718c19ab0dc41f2981854921e
Robert Chang <robccorp@gmail.com>
2018-09-30 20:08:09 -0400

    added methods to lion and gorilla class

--- src/main/java/cscie55/hw2/zoo/animals/Gorilla.java
+++ src/main/java/cscie55/hw2/zoo/animals/Gorilla.java
@@ -1,4 +1,4 @@
-package cscie55.zoo.animals;
+package main.java.cscie55.hw2.zoo.animals;
 
 /************************************************************ 
 * HW 1, Part II - Create 5 classes in package cscie55.zoo.animals.
@@ -10,35 +10,62 @@ public class Gorilla {
     
     private String name;
     private int age;
-    private boolean gender;
+    private boolean male;
     ArrayList<String> favoriteFoods = new ArrayList<String>();
     public enum Diet { VEGGIE, MEAT, OMNI };
     Diet diet;
     
-/**Takes no arguments and initializes the name,age,gender,foods,diet to generic starting values.
+/**Takes no arguments and initializes the name,age,male,foods,diet to general starting values.
  * 
  */
     public Gorilla(){
         this.name = "gorilla";
         this.age = 0;
-        this.gender = true;
-        this.favoriteFoods.add("water");
+        this.male = true;
+        this.favoriteFoods.add("bananas");
+        this.favoriteFoods.add("leaves");
+        this.favoriteFoods.add("yams");
         this.diet = Diet.VEGGIE;
     }
 
     public String toString(){
-        return getClass().getName()+"[name="+this.name+",age="+this.age+",gender="+this.gender+",favfood="+this.favoriteFoods.get(0)+",diet="+this.diet+"]";
+        return getClass().getName()+"[name="+this.name+",age="+this.age+",male="+this.male+",favfood="+this.favoriteFoods.get(0)+",diet="+this.diet+"]";
     }
 
-/**Uses toString to test the class.
- * 
- */     
-    public static void main(String[] argv){
-        System.out.println("");
-		System.out.println("");
-        Gorilla gorilla_1 = new Gorilla();
-        System.out.println(gorilla_1.toString());
+/**Returns a String that takes the favoriteFoods data from
+ * Assignment 1 and displays it as proof that it is eating well.
+ */    
+public String eat(){
+    StringBuilder eating = new StringBuilder();
+    eating.append("Delightfully tasty ");
+    for(int i = 0; i < favoriteFoods.size(); i++) {
+        eating.append(favoriteFoods.get(i));
+        eating.append(i < favoriteFoods.size() - 1 ? ", " : ".");
     }
+    return eating.toString();
+}
+
+/**Return a String indicating what sound the animal makes.
+*/ 
+public String speak(){
+    StringBuilder speaking = new StringBuilder("Chee Chee!");
+    return speaking.toString();
+}
+
+/**Return a String indicating what sound the animal makes while playing.
+*/  
+public String play(){
+    StringBuilder playing = new StringBuilder("Go Go Gorillas!");
+    return playing.toString();
+}
+
+public static void main(String[] argv){
+    Gorilla gorilla_1 = new Gorilla();
+    System.out.println("");
+    System.out.println(gorilla_1.eat());
+    System.out.println(gorilla_1.speak());
+    System.out.println(gorilla_1.play());
+}
 
 
 }
--- src/main/java/cscie55/hw2/zoo/animals/Lion.java
+++ src/main/java/cscie55/hw2/zoo/animals/Lion.java
@@ -1,4 +1,4 @@
-package cscie55.zoo.animals;
+package main.java.cscie55.hw2.zoo.animals;
 
 /************************************************************ 
 * HW 1, Part II - Create 5 classes in package cscie55.zoo.animals.
@@ -10,35 +10,62 @@ public class Lion {
     
     private String name;
     private int age;
-    private boolean gender;
+    private boolean male;
     ArrayList<String> favoriteFoods = new ArrayList<String>();
     public enum Diet { VEGGIE, MEAT, OMNI };
     Diet diet;
     
-/**Takes no arguments and initializes the name,age,gender,foods,diet to generic starting values.
+/**Takes no arguments and initializes the name,age,male,foods,diet to general starting values.
  * 
  */
     public Lion(){
         this.name = "lion";
         this.age = 0;
-        this.gender = true;
-        this.favoriteFoods.add("water");
-        this.diet = Diet.VEGGIE;
+        this.male = true;
+        this.favoriteFoods.add("beef");
+        this.favoriteFoods.add("chicken");
+        this.favoriteFoods.add("pork");
+        this.diet = Diet.MEAT;
     }
 
     public String toString(){
-        return getClass().getName()+"[name="+this.name+",age="+this.age+",gender="+this.gender+",favfood="+this.favoriteFoods.get(0)+",diet="+this.diet+"]";
+        return getClass().getName()+"[name="+this.name+",age="+this.age+",male="+this.male+",favfood="+this.favoriteFoods.get(0)+",diet="+this.diet+"]";
     }
 
-/**Uses toString to test the class.
- * 
- */     
-    public static void main(String[] argv){
-        System.out.println("");
-		System.out.println("");
-        Lion lion_1 = new Lion();
-        System.out.println(lion_1.toString());
+/**Returns a String that takes the favoriteFoods data from
+ * Assignment 1 and displays it as proof that it is eating well.
+ */    
+public String eat(){
+    StringBuilder eating = new StringBuilder();
+    eating.append("Delightfully tasty ");
+    for(int i = 0; i < favoriteFoods.size(); i++) {
+        eating.append(favoriteFoods.get(i));
+        eating.append(i < favoriteFoods.size() - 1 ? ", " : ".");
     }
+    return eating.toString();
+}
+
+/**Return a String indicating what sound the animal makes.
+*/ 
+public String speak(){
+    StringBuilder speaking = new StringBuilder("Roar!");
+    return speaking.toString();
+}
+
+/**Return a String indicating what sound the animal makes while playing.
+*/  
+public String play(){
+    StringBuilder playing = new StringBuilder("Lion's Lair!");
+    return playing.toString();
+}
+
+public static void main(String[] argv){
+    Lion lion_1 = new Lion();
+    System.out.println("");
+    System.out.println(lion_1.eat());
+    System.out.println(lion_1.speak());
+    System.out.println(lion_1.play());
+}
 
 
 }
